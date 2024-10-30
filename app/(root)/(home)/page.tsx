942c079403d66f92868d4995da1fa3490393cb57
VK-10 <visu2266@gmail.com>
2024-10-30 23:48:45 +0530

    first commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,16 @@
+//import { sidebarLinks } from '@/constants'
+import { Sidebar } from 'lucide-react'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className = "flex size-full flex-col gap-10 text-black">
+        <h1 className='text-3xl font-bold'>
+            Home
+        </h1>
+
+    </section>
+  )
+}
+
+export default Home
